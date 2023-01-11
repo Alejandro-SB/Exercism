ca1fd92a2e6dfc90644178d22cc9dd6608ed9d80
Alejandro <alejandro.sanchez.bastida@gmail.com>
2023-01-11 20:30:47 +0100

    Add Rust exercises

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,28 +1,34 @@
-#[derive(Debug, PartialEq, Eq)]
+use enum_iterator::{all, Sequence};
+use int_enum::IntEnum;
+
+#[repr(u32)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, IntEnum, Sequence)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
 }
 
 pub fn color_to_value(_color: ResistorColor) -> u32 {
-    unimplemented!("convert a color into a numerical representation")
+    _color.int_value()
 }
 
 pub fn value_to_color_string(value: u32) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    let color_result = ResistorColor::from_int(value);
+
+    match color_result {
+        Ok(color) => format!("{:?}", color),
+        Err(_e) => "value out of range".to_string(),
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    all::<ResistorColor>().collect::<Vec<_>>()
 }
